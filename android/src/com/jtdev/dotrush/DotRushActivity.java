54aa0c4269acbff81d833d7b5c9992d5a8302981
cyphar <cyphar@cyphar.com>
2014-02-18 19:39:21 +1100

    android: hide virtual buttons

--- android/src/com/jtdev/dotrush/DotRushActivity.java
+++ android/src/com/jtdev/dotrush/DotRushActivity.java
@@ -19,26 +19,8 @@ public class DotRushActivity extends AndroidApplication {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        View focus = getCurrentFocus();
-
-        if(focus != null)
-            focus.setSystemUiVisibility(getHiddenUiFlag());
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
 
         initialize(new DotRush(), cfg);
-    }
-
-    private int getHiddenUiFlag() {
-        /* since < ICS, you couldn't completely hide the buttons,
-         * there needs to be a compatibility layer to deal with this */
-
-        int hidden = View.SYSTEM_UI_FLAG_VISIBLE;
-
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-            hidden = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
-            hidden = View.STATUS_BAR_HIDDEN;
-
-        return hidden;
     }
 }
